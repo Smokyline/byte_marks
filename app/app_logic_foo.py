13dc996be08c147882062da13e74bfe1b70226a7
Ivan Belov <i.belov@gcras.ru>
2023-04-07 16:19:16 +0300

    its work

--- app/app_logic_foo.py
+++ app/app_logic_foo.py
@@ -18,7 +18,6 @@ comp_mark_value = {
     'f':1
 }
 
-
 def post_marks_to_sql(rq):
     """
         постинг в SQL таблицу
@@ -28,55 +27,69 @@ def post_marks_to_sql(rq):
     date1 = int(rq['date1'])
     marks_action = int(rq['action'])
     vector_comp = str(rq['comp']).lower()
+    frec = str(rq['frec']).lower()
 
-    """
-    floatlist = [random.random() for _ in range(10**5)]
-    buf = struct.pack('%sf' % len(floatlist), *floatlist)
-    """
-    edit_min_range = np.arange(date0, date1+1, 60)
-    conn = pymysql.connect(host=os.getenv('SQL_HOST'), port=3306,
-                           user=os.getenv('SQL_USER'),
-                           passwd=os.getenv('SQL_PSW'),
-                           db=os.getenv('SQL_DB'),
-                           charset='utf8mb4', )
-    cur = conn.cursor()
-    date0_round = date0//3600*3600
-    for dt_hour in np.arange(date0_round, date1 + 1, 60 * 60):  # for every hours
-        request_to_db = "SELECT date, bad FROM %s WHERE code='%s' " \
-                        "AND date=%i" % (os.getenv('SQL_TABLE'), obs_code, dt_hour)
+    if frec == 'min':
+        tick = 60
+        table_name = os.getenv('SQL_TABLE_MIN')
+    else:
+        tick = 1
+        table_name = os.getenv('SQL_TABLE_SEC')
 
-        cur.execute(request_to_db)
-        respond = cur.fetchall()[0]
-        sql_date, sql_bad = respond
+    # создаем массив меток времени, где нужно ввести пометки
+    edit_min_range = np.arange(date0, date1+1, tick)
 
-        # если это первая запись
-        if sql_bad is None:
-            bad_array = np.zeros(60).astype(int)
+    try:
+        conn = pymysql.connect(host=os.getenv('SQL_HOST'), port=3306,
+                               user=os.getenv('SQL_USER'),
+                               passwd=os.getenv('SQL_PSW'),
+                               db=os.getenv('SQL_DB'),
+                               charset='utf8mb4', autocommit=False)
+        cur = conn.cursor()
 
-        # если нужно обновить строку
-        else:
-            fmt = '>B'
-            bad_array = np.array(list(struct.iter_unpack(fmt, sql_bad)),
-                                    dtype=np.int).squeeze()
+        # бежим по каждой строке sql таблицы, строка=час
+        date0_round = date0 // 3600 * 3600
+        for dt_hour in np.arange(date0_round, date1 + 1, 60 * 60):  # for every hour
+            request_to_db = "SELECT date, bad FROM %s WHERE code='%s' " \
+                            "AND date=%i" % (table_name, obs_code, dt_hour)
 
-        for i, dt_min in enumerate(np.arange(dt_hour, dt_hour + (60 * 60), 60)):
-            if dt_min in edit_min_range:
-                if marks_action:
-                    bad_array[i] |= comp_mark_value[vector_comp]
+            cur.execute(request_to_db)
+            respond = cur.fetchall()[0]
+            sql_date, sql_bad = respond
+
+            # если это первая запись
+            if sql_bad is None:
+                if frec == 'min':
+                    bad_array = np.zeros(60).astype(int)
                 else:
-                    bad_array[i] &= ~comp_mark_value[vector_comp]
+                    bad_array = np.zeros(3600).astype(int)
 
-        sql_upd_request = """UPDATE %s SET """ % str(os.getenv('SQL_TABLE')) + """bad=%s""" \
-                          + """ WHERE code='%s' AND date=%i""" % (obs_code, dt_hour)
+            # если нужно обновить строку
+            else:
+                fmt = '>B'
+                bad_array = np.array(list(struct.iter_unpack(fmt, sql_bad)),
+                                     dtype=np.int).squeeze()
 
-        print(bad_array)
-        print(sql_upd_request)
-        insert_array_byte = struct.pack('>%sB' % len(bad_array), *bad_array)
-        insert_tuple = (insert_array_byte)
-        cur.execute(sql_upd_request, insert_tuple)
-        conn.commit()
-        conn.close()
-        return 0
+            # обновляем bad_array
+            for i, dt_min in enumerate(np.arange(dt_hour, dt_hour + (60 * 60), tick)):
+                if dt_min in edit_min_range:
+                    if marks_action:
+                        bad_array[i] |= comp_mark_value[vector_comp]
+                    else:
+                        bad_array[i] &= ~comp_mark_value[vector_comp]
+
+            # постим в таблицу
+            sql_upd_request = """UPDATE %s SET """ % table_name + """bad=%s""" \
+                              + """ WHERE code='%s' AND date=%i""" % (obs_code, dt_hour)
+            insert_array_byte = struct.pack('>%sB' % len(bad_array), *bad_array)
+            insert_tuple = (insert_array_byte)
+            cur.execute(sql_upd_request, insert_tuple)
+            conn.commit()
+            conn.close()
+            return 0
+    except Exception as e:
+        print(e)
+        return 1
 
 
 
@@ -86,7 +99,6 @@ def get_marks_from_sql(rq):
     obs_code = str(rq['code']).upper()
     # obs_code = 'IRT'
     date = int(rq['date'])
-    orient = str(rq['orient']).upper()
     conn = pymysql.connect(host=os.getenv('SQL_HOST'), port=3306,
                            user=os.getenv('SQL_USER'),
                            passwd=os.getenv('SQL_PSW'),
@@ -94,7 +106,7 @@ def get_marks_from_sql(rq):
                            charset='utf8mb4', )
     cur = conn.cursor()
     request_to_db = "SELECT * FROM %s WHERE code='%s' " \
-                    "AND date=%i AND orient='%s'" % (os.getenv('SQL_TABLE'), obs_code, date, orient)
+                    "AND date=%i AND " % (os.getenv('SQL_TABLE'), obs_code, date)
 
     cur.execute(request_to_db)
     respond = cur.fetchall()[0]
